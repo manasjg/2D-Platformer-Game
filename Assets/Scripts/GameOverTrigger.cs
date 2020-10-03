f1e5f32b40bbb7fecf0670f050f415df97142779
Manas Gangwani <manasjg1996@gmail.com>
2020-10-03 15:09:58 +0530

    Added Game Over Trigger at bottom of level

---
+++ Assets/Scripts/GameOverTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Triggers
+{
+    public class GameOverTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+                Debug.Log("Game Over");
+        }
+    }
+}
