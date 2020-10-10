0d91d1d8f711cd53f681852ddd79fab8d4ca120b
Manas Gangwani <manasjg1996@gmail.com>
2020-10-10 15:36:48 +0530

    Created Lobby screen with multiple levels

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField]
+    string levelName;
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(LoadScene);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
